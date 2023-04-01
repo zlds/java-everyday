6d2ad25f75e6ab11afeb6e4bca33b2526eeb7ec0
hanchaowei <hanchaowei0@gmail.com>
2023-04-01 11:44:33 +0800

    添加oauth2模块

---
+++ oauth-server/src/main/java/org/example/OauthServerApplication.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: hanchaowei
+ * @date 2023/4/1
+ * @description:
+ */
+@SpringBootApplication
+public class OauthServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OauthServerApplication.class,args);
+	}
+}
