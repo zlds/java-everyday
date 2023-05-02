cdea428e1dcda7af00556b3ec7c18dc41601a773
hanchaowei <hanchaowei0@gmail.com>
2023-05-02 17:29:11 +0800

    代理模式两种实现方式

---
+++ daily-learning/src/main/java/org/example/gof/proxymodel/Image.java
@@ -0,0 +1,11 @@
+package org.example.gof.proxymodel;
+
+/**
+ * @author: hanchaowei
+ * @date 2023/5/2
+ * @description:
+ */
+
+public interface Image {
+	void display();
+}
---
+++ daily-learning/src/main/java/org/example/gof/proxymodel/ProxyPatternDemo.java
@@ -0,0 +1,21 @@
+package org.example.gof.proxymodel;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * @author: hanchaowei
+ * @date 2023/5/2
+ * @description:
+ */
+
+public class ProxyPatternDemo {
+	public static void main(String[] args) {
+		Image image = (Image) Proxy.newProxyInstance(
+				Image.class.getClassLoader(),
+				new Class[]{Image.class},
+				new ImageInvocationHandler("test.jpg")
+		);
+
+		image.display();
+	}
+}
