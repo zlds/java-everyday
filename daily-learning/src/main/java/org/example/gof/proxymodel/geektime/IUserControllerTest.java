9e502038fbe58fd162be462b38cc5dabc3c6b59c
hanchaowei <hanchaowei0@gmail.com>
2023-05-03 11:32:58 +0800

    geektime代理模式动态代理

--- daily-learning/src/main/java/org/example/gof/proxymodel/geektime/IUserControllerTest.java
+++ daily-learning/src/main/java/org/example/gof/proxymodel/geektime/IUserControllerTest.java
@@ -23,5 +23,15 @@ public class IUserControllerTest {
 		 */
 		UserControllerProxyExtend userControllerProxyExtend = new UserControllerProxyExtend();
 		userControllerProxyExtend.login("123", "123");
+
+		/**
+		 * 高阶使用，通过动态代理实现。
+		 * 为什么要通过动态代理：如果我们有50个要扩展功能的原始类，那我们就需要创建50个对应的代理类，这会导致项目中的类成倍增加，增加了代码维护成本。
+		 * 动态代理的方式，只需要创建一个动态代理类，通过传入不同的原始类对象，就可以创建不同的动态代理类对象，这样就避免了创建过多的代理类。
+		 */
+		//MetricsCollectorProxy使用举例
+		MetricsCollectorProxy proxy = new MetricsCollectorProxy();
+		IUserController userController = (IUserController) proxy.createProxy(new UserController());
+
 	}
 }
