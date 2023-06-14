cee39b1ab19805a012e2f66ad2d56ef3328483a3
hanchaowei <hanchaowei0@gmail.com>
2023-06-14 20:09:51 +0800

    使用数组创建链表

--- daily-learning/src/main/java/org/example/binarytree/ListNodeTest.java
+++ daily-learning/src/main/java/org/example/binarytree/ListNodeTest.java
@@ -22,6 +22,29 @@ public class ListNodeTest {
 		}
 	}
 
+	/**
+	 * 使用数组创建链表
+	 * @param arr
+	 * @return
+	 */
+	public static ListNode createList(int[] arr) {
+		if (arr == null || arr.length == 0) {
+			return null;
+		}
+		// 头结点
+		ListNode head = new ListNode(arr[0]);
+		// 当前节点
+		ListNode curr = head;
+		// 遍历数组
+		for (int i = 1; i < arr.length; i++) {
+			// 创建新节点，值为当前元素
+			curr.next = new ListNode(arr[i]);
+			// 当前节点指向下一个节点
+			curr = curr.next;
+		}
+		return head;
+	}
+
 
 	public static void main(String[] args) {
 		// 创建链表 1->2->3->4->5
@@ -32,5 +55,11 @@ public class ListNodeTest {
 		head.next.next.next.next = new ListNode(5);
 
 		printList(head);
+
+		// 使用数组创建链表
+		int[] ints = {5,4,3,2,1};
+		ListNode list = createList(ints);
+		System.out.println("==============");
+		printList(list);
 	}
 }
