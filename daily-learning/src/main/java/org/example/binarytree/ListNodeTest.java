f4e106bb0194a388185427546f3608e2c9848c99
hanchaowei <hanchaowei0@gmail.com>
2023-06-15 19:38:41 +0800

    链表插入和删除学习

--- daily-learning/src/main/java/org/example/binarytree/ListNodeTest.java
+++ daily-learning/src/main/java/org/example/binarytree/ListNodeTest.java
@@ -45,6 +45,64 @@ public class ListNodeTest {
 		return head;
 	}
 
+	/**
+	 * 在指定位置插入新的节点
+	 * @param head
+	 * @param index
+	 * @param val
+	 * @return
+	 */
+	public static ListNode insertAt(ListNode head, int index,int val) {
+		// 虚拟头节点
+		ListNode virtualHead = new ListNode(-1);
+		// 将虚拟头结点的下一个节点执行实际的头节点
+		virtualHead.next = head;
+
+		// 当前节点
+		ListNode curr = virtualHead;
+
+		// 找出要插入位置的前一个节点。如1->2->3->4->5，要在2和3之间插入新节点，则找到2
+		for (int i = 0; i < index; i++) {
+			if (curr.next != null) {
+				curr = curr.next;
+			} else {
+				throw new IllegalArgumentException("index out of bounds");
+			}
+		}
+		// 创建新节点
+		ListNode newNode = new ListNode(val);
+		// 新节点的下一个节点指向当前节点的下一个节点
+		newNode.next = curr.next;
+		// 当前节点的下一个节点指向新节点
+		curr.next = newNode;
+		return virtualHead.next;
+	}
+
+
+	public static ListNode deleteAt(ListNode head, int index) {
+		// 虚拟头结点
+		ListNode virtualHead = new ListNode(-1);
+		virtualHead.next = head;
+
+		ListNode current = virtualHead;
+
+		// 找到要删除节点的前一个节点
+		for (int i = 0; i < index; i++) {
+			if (current.next != null) {
+				current = current.next;
+			} else {
+				throw new IllegalArgumentException("index out of bounds");
+			}
+		}
+
+		// 删除节点
+		if (current.next != null) {
+			current.next = current.next.next;
+		}
+		return virtualHead.next;
+
+	}
+
 
 	public static void main(String[] args) {
 		// 创建链表 1->2->3->4->5
@@ -57,9 +115,24 @@ public class ListNodeTest {
 		printList(head);
 
 		// 使用数组创建链表
+		System.out.println("==============");
 		int[] ints = {5,4,3,2,1};
 		ListNode list = createList(ints);
-		System.out.println("==============");
 		printList(list);
+
+		// 测试在指定位置插入数据
+		System.out.println("======= 在指定位置插入测试 =======");
+		int[] arrInsert = {1,2,3,4,5};
+		ListNode updateListNode = createList(arrInsert);
+		ListNode insertResult = insertAt(updateListNode, 2, 100);
+		printList(insertResult);
+
+
+		// 测试在指定位置删除数据
+		System.out.println("======= 在指定位置删除测试 =======");
+		int[] arrDelete = {1,2,3,4,5};
+		ListNode deleteListNode = createList(arrDelete);
+		ListNode deleteResult = deleteAt(deleteListNode, 2);
+		printList(deleteResult);
 	}
 }
