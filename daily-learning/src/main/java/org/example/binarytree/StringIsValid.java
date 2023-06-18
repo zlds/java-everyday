2ff4a96db6a219481768982acf5fa1d71f2379c9
hanchaowei <hanchaowei0@gmail.com>
2023-06-18 14:20:17 +0800

    判断括号字符串是否有效

---
+++ daily-learning/src/main/java/org/example/binarytree/StringIsValid.java
@@ -0,0 +1,46 @@
+package org.example.binarytree;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author: hanchaowei
+ * @date 2023/6/18
+ * @description:
+ */
+
+public class StringIsValid {
+
+
+	/**
+	 * 判断括号字符串是否有效
+	 * @param s
+	 * @return
+	 */
+	public static boolean isValid(String s) {
+		Deque<Character> stack = new LinkedList<>();
+		// 循环遍历
+		for (char c : s.toCharArray()) {
+			// 如果是左括号压入栈中。如果是右括号先检查栈是否为空，并查看栈顶的元素是不是对应的开括号，如果是弹出
+			if (c == '(' || c == '[' || c == '{') {
+				stack.push(c);
+			} else if (c == ')' && !stack.isEmpty() || stack.peek() == ')') {
+				stack.pop();
+			} else if (c == ']' && !stack.isEmpty() || stack.peek() == ']') {
+				stack.pop();
+			} else if (c == '}' && !stack.isEmpty() || stack.peek() == '}') {
+				stack.pop();
+			} else {
+				return false;
+			}
+		}
+		return stack.isEmpty();
+	}
+
+
+	public static void main(String[] args) {
+		String s = "([]){}";
+		System.out.println(isValid(s));
+	}
+
+}
