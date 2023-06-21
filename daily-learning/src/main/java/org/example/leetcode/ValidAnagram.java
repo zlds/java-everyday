9b4a45238686c52ad2b110e242c5cbd2cd5675c1
hanchaowei <hanchaowei0@gmail.com>
2023-06-21 17:59:51 +0800

    有效的字母异位词新增两种写法

--- daily-learning/src/main/java/org/example/leetcode/ValidAnagram.java
+++ daily-learning/src/main/java/org/example/leetcode/ValidAnagram.java
@@ -1,5 +1,9 @@
 package org.example.leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author: hanchaowei
  * @date 2023/6/20
@@ -8,7 +12,7 @@ package org.example.leetcode;
 
 public class ValidAnagram {
 
-	public static boolean ifStrAnagram(String s1, String s2) {
+	public static boolean isStrAnagram(String s1, String s2) {
 		// 如果两个字符串长度不相等，直接返回false
 		if (s1.length() != s2.length()) {
 			return false;
@@ -23,6 +27,7 @@ public class ValidAnagram {
 			 * 这段代码的意思是：字符串a的ASCII码是97,字符串b的ASCII码是98，那么a-b的值就是1，然后将1的结果作为索引
 			 * ++的意思是如果字符串a中有两个a，那么counter[0]的值就是2
 			 * --的意思是如果字符串相同，就会减掉，如果字符串不同，就会加上。最终元素中的所有元素都应该是0，因为每个字符串出现的参数都相同
+			 * 为什么是-a: 是为了将所有的小写字母转换为一个索引范围从0到25。'a' - 'a' = 0 ;直到'z' - 'a' = 25
 			 */
 			counter[s1.charAt(i) - 'a']++;
 			counter[s2.charAt(i) - 'a']--;
@@ -38,11 +43,55 @@ public class ValidAnagram {
 
 	}
 
+
+	/**
+	 * 字母
+	 * @param s1
+	 * @param s2
+	 * @return
+	 */
+	public static boolean isAnagram1(String s1, String s2) {
+		if (s1.length() != s2.length()) {
+			return false;
+		}
+		// 转换成字符数组
+		char[] charsA = s1.toCharArray();
+		char[] charsB = s2.toCharArray();
+
+		Arrays.sort(charsA);
+		Arrays.sort(charsB);
+		return Arrays.equals(charsA,charsB);
+	}
+
+	public static boolean isAnagram2(String s1, String s2) {
+		if (s1.length() != s2.length()) {
+			return false;
+		}
+		Map<Character,Integer> count = new HashMap<>();
+		for (int i = 0; i < s1.length(); i++) {
+			char s = s1.charAt(i);
+			char t = s2.charAt(i);
+			count.put(s,count.getOrDefault(s,0) + 1);
+			count.put(t,count.getOrDefault(t,0) - 1);
+		}
+
+		for (char c : count.keySet()) {
+			if (count.get(c) != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
 		// ture
-		System.out.println("tar 和art 比较结果: "+ ifStrAnagram("tar", "art"));
+		System.out.println("tar 和art 比较结果: "+ isStrAnagram("tar", "art"));
 		// false
-		System.out.println("ta 和art 比较结果: "+ ifStrAnagram("ta", "art"));
+		System.out.println("ta 和art 比较结果: "+ isStrAnagram("ta", "art"));
+		// true
+		System.out.println(isAnagram1("tar", "art"));
+		// true
+		System.out.println(isAnagram2("tar","art"));
 
 	}
 }
